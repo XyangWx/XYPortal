70b9e0a544251f27a74eddfa82ad871082ba7304
许阳 <mksword@126.com>
2026-01-22 11:13:10 +0800

    关闭注册，新增Test功能

---
+++ XYPortal/src/XYPortal.Domain/Features/XYPortalFeatures.cs
@@ -0,0 +1,15 @@
+namespace XYPortal.Features;
+
+public static class XYPortalFeatures
+{
+    public const string GroupName = "XYPortal";
+
+    public static class Test
+    {
+        public const string Default = GroupName + ".Test";
+        public const string Enable = Default + ".Enable";
+        public const string NumbValue = Default + ".NumbValue";
+        public const string DecimalValue = Default + ".DecimalValue";
+        public const string StringValue = Default + ".StringValue";
+    }
+}
