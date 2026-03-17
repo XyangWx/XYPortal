e7eaa6a085ceac518a0cb124f2385b8cf7cba25b
许阳 <xuyang_wx@aliyun.com>
2026-03-17 14:34:33 +0800

    补上缺少的项目

--- XYPortal.RandomStringProvider/src/XYPortal.RandomStringProvider.Application/RandomStringProvider/UnicodeMiscSymbolsPool.cs
+++ XYPortal.RandomStringProvider/src/XYPortal.RandomStringProvider.Application/RandomStringProvider/UnicodeMiscSymbolsPool.cs
@@ -6,7 +6,7 @@ namespace XYPortal.RandomStringProvider.RandomStringProvider;
 
 internal class UnicodeMiscellaneousPool : SymbolPool
 {
-    private static readonly char[] Chars = "".ToCharArray();
+    private static readonly char[] Chars = GetMiscellaneousSymbols();
     
     public UnicodeMiscellaneousPool(Random random)
         : base(random)
@@ -17,6 +17,11 @@ internal class UnicodeMiscellaneousPool : SymbolPool
     {
         var chars = Chars.Where(c => !ignores.Contains(c)).ToArray();
         
+        if (chars.Length == 0)
+        {
+            throw new InvalidOperationException("No available characters after filtering ignores");
+        }
+        
         var value = chars[Next(chars.Length)];
 
         return value;
@@ -24,20 +29,21 @@ internal class UnicodeMiscellaneousPool : SymbolPool
 
     private static char[] GetMiscellaneousSymbols()
     {
-        int start1 = 0x2600;
-        int start2 = 0x2638;
+        int start1 = 0x2600;  // Miscellaneous Symbols (☀ to ⛰)
+        int start2 = 0x2638;  // Resume and Select symbols (☸ to ⛿)
 
         List<char> chars = [];
 
+        // Miscellaneous Symbols range (0x2600 - 0x26FF)
         for (int i = 0; i < 0x30; i++)
         {
-            chars.Add((char)(start1 + 1));
+            chars.Add((char)(start1 + i));
         }
 
-        // 避开八卦段
-        for (int i = 0; i < 0xc8; i++)
+        // Resume and Select symbols (0x2638 - 0x27BF)
+        for (int i = 0; i < 0x88; i++)
         {
-            chars.Add((char)(start2 + 1));
+            chars.Add((char)(start2 + i));
         }
         
         return chars.ToArray();
